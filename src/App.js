77e8293f6c776cb8db5a78bd9cabf415d497d6d2
Niédja Bezerra <niedjacarlos@gmail.com>
2022-02-14 00:58:37 -0300

    react 04 - practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const text = () => {
+  return "HelloWorld"
+}
+const number = (n) => { 
+  return n*2;
 }
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>{number(5)}</div>
+        <div>{text()}</div>
+      </div>
+    )
+  }
+}
+
